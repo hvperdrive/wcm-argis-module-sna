55d29f697b1e648133dae0023630fb14df71cd4b
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2019-04-29 17:45:18 +0200

    perf: Better error handling (2)

---
+++ app/helpers/adapters/helpers/responseParser.js
@@ -0,0 +1,27 @@
+module.exports = (response) => {
+    if(!response) {
+        return response;
+    }
+
+    if (response.error) {
+        throw response.error;
+    }
+
+    if(Array.isArray(response.addResults)) {
+        const errors = response.addResults.filter((result) => result.success === false);
+
+        if (errors.length) {
+            throw errors;
+        }
+    }
+
+    if(Array.isArray(response.deleteResults)) {
+        const errors = response.deleteResults.filter((result) => result.success === false);
+
+        if (errors.length) {
+            throw errors;
+        }
+    }
+
+    return response;
+};
