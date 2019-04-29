f5120117cc4b20f5f703f42bb7ba882d8bdedf4c
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2019-04-29 17:49:24 +0200

    perf: use Q.allSettled instead of Promise all in resync functionality

--- app/helpers/contentSync/handlers/upsert.js
+++ app/helpers/contentSync/handlers/upsert.js
@@ -1,3 +1,4 @@
+const Q = require("q");
 const { prop } = require("ramda");
 const validators = require("../validators");
 
@@ -7,20 +8,39 @@ const emitters = require("../emitters");
 
 module.exports = (content) => {
 	if (!validators.content(content)) {
-		console.log(`SNA-ARCGIS-MODULE: Invalid content, skipping arcgis sync for ${prop("_id", content)}`);
+		console.log(`SNA-ARCGIS-MODULE: Invalid content, skipping arcgis sync for ${prop("uuid", content)}`);
 		return;
-	}
+    }
 
 	// Get existing polygons & points of the content item if there are any
 	return fetchers.getArcgisFeaturesByContent(content)
 		// Sort existing p&p's en content shapes into appropriate actions
 		.then((arcgisFeatures) => mappers.sortShapesInOperations(content, arcgisFeatures))
 		// Exec sync actions
-		.then((operations) => Promise.all([
+		.then((operations) => Q.allSettled([
 			emitters.remove(operations.remove),
 			emitters.update(operations.update),
 			emitters.create(operations.create),
-		]))
+        ]))
+            .then((responses) => {
+                const errors = responses.filter((promiseResult) => promiseResult.state !== "fulfilled");
+
+                if (errors.length) {
+                    throw errors;
+                }
+
+                return responses;
+            })
 			.then((result) => console.log("SNA-ARCGIS-MODULE: Sync successfull") || result)
-			.catch((error) => console.log(`SNA-ARCGIS-MODULE: Sync error for ${prop("_id", content)}`, error))
+			.catch((error) => {
+                let parsedError;
+
+                try {
+                    parsedError = JSON.stringify(error);
+                } catch (e) {
+                    parsedError = error;
+                }
+
+                console.log(`SNA-ARCGIS-MODULE: Sync error for ${prop("uuid", content)}`, parsedError);
+            })
 };
