2760b48217a89838215c5db9294633170bc3b963
Jeroen Valcke <jeroen.valcke@studiohyperdrive.be>
2019-05-03 13:13:18 +0200

    Set max length on strings

--- app/helpers/contentSync/mappers/shapeToFeature.js
+++ app/helpers/contentSync/mappers/shapeToFeature.js
@@ -1,18 +1,32 @@
-const { path, set, compose, lensProp, prop, omit } = require("ramda");
+const {
+	__,
+	compose,
+	lensProp,
+	identity,
+	invoker,
+	ifElse,
+	length,
+	curry,
+	path,
+	prop,
+	omit,
+	set,
+	gt,
+} = require("ramda");
 
 const getMappedGeometry = (shape) => {
 	switch(shape.geometry.type) {
 		// Set polygon geometry
 		case "Polygon":
 			return compose(
-                omit(["coordinates", "type"]),
-                set(lensProp("rings"), shape.geometry.coordinates)
-            )(shape.geometry);
+				omit(["coordinates", "type"]),
+				set(lensProp("rings"), shape.geometry.coordinates)
+			)(shape.geometry);
 
 		// Set point geometry
 		case "Point":
 			return compose(
-                omit(["coordinates", "type"]),
+				omit(["coordinates", "type"]),
 				set(lensProp("x"), shape.geometry.coordinates[0]),
 				set(lensProp("y"), shape.geometry.coordinates[1])
 			)(shape.geometry)
@@ -21,24 +35,36 @@ const getMappedGeometry = (shape) => {
 	}
 };
 
+const getStringWithMaxLength = curry((maxLength, p, object) => compose(
+	ifElse(
+		compose(gt(__, maxLength), length),
+		compose(
+		  (val) => val + "..." ,
+		  invoker(2, 'substring')(0, maxLength - 3)
+		),
+		identity
+	),
+	path(p)
+)(object));
+
 module.exports = (content, shape) => ({
 	type: "Feature",
 	geometry: Object.assign(
-        {},
-        {
-            spatialReference: {
-                wkid: 4326,
-            }
-        },
-        getMappedGeometry(shape)
-    ),
+		{},
+		{
+			spatialReference: {
+				wkid: 4326,
+			}
+		},
+		getMappedGeometry(shape)
+	),
 	attributes: {
 		F_id: prop("uid")(shape),
 		uuid: prop("uuid")(content),
-		title: path(["fields", "title", "nl"])(content),
-		intro: path(["fields", "metaDescription", "nl"])(content),
+		title: getStringWithMaxLength(150, ["fields", "title", "nl"])(content),
+		intro: getStringWithMaxLength(255, ["fields", "metaDescription", "nl"])(content),
 		lastModified: path(["meta", "lastModified"])(content),
 		created: path(["meta", "created"])(content),
-		link: path(["meta", "slug", "nl"])(content),
+		link: getStringWithMaxLength(255, ["meta", "slug", "nl"])(content),
 	}
 });
