1c5bf8fa2ebaf48b7d66d439999f2245065ded29
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2020-11-19 14:48:16 +0100

    feat: check on published

--- app/helpers/contentSync/handlers/upsert.js
+++ app/helpers/contentSync/handlers/upsert.js
@@ -1,5 +1,5 @@
 const Q = require("q");
-const { prop } = require("ramda");
+const { prop, pathOr } = require("ramda");
 const validators = require("../validators");
 
 const mappers = require("../mappers");
@@ -7,7 +7,7 @@ const fetchers = require("../fetchers");
 const emitters = require("../emitters");
 
 module.exports = (content) => {
-	if (!validators.content(content)) {
+	if (!validators.content(content) || pathOr(false, ['meta', 'published'])(content) === false) {
 		console.log(`SNA-ARCGIS-MODULE: Invalid content, skipping arcgis sync for ${prop("uuid", content)}`);
 		return;
 	}
