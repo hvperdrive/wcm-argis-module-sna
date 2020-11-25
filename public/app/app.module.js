63c2cb55aed1b72965d70fb27e7d965019568b2f
Pieterjan Van Saet <pieterjan.vansaet@studiohyperdrive.be>
2020-11-25 16:07:02 +0100

    feat: bump

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,17 +1,17 @@
 (function(angular) {
-	angular.module("wcm-arcgis-sna_1.1.0.factories", []);
-	angular.module("wcm-arcgis-sna_1.1.0.services", ["wcm-arcgis-sna_1.1.0.factories"]);
-	angular.module("wcm-arcgis-sna_1.1.0.controllers", ["wcm-arcgis-sna_1.1.0.services"]);
-	angular.module("wcm-arcgis-sna_1.1.0.directives", ["wcm-arcgis-sna_1.1.0.controllers"]);
+	angular.module("wcm-arcgis-sna_1.2.0.factories", []);
+	angular.module("wcm-arcgis-sna_1.2.0.services", ["wcm-arcgis-sna_1.2.0.factories"]);
+	angular.module("wcm-arcgis-sna_1.2.0.controllers", ["wcm-arcgis-sna_1.2.0.services"]);
+	angular.module("wcm-arcgis-sna_1.2.0.directives", ["wcm-arcgis-sna_1.2.0.controllers"]);
 
-	angular.module("wcm-arcgis-sna_1.1.0", [
+	angular.module("wcm-arcgis-sna_1.2.0", [
 
 		"pelorus.services",
 
-		"wcm-arcgis-sna_1.1.0.factories",
-		"wcm-arcgis-sna_1.1.0.services",
-		"wcm-arcgis-sna_1.1.0.controllers",
-		"wcm-arcgis-sna_1.1.0.directives"
+		"wcm-arcgis-sna_1.2.0.factories",
+		"wcm-arcgis-sna_1.2.0.services",
+		"wcm-arcgis-sna_1.2.0.controllers",
+		"wcm-arcgis-sna_1.2.0.directives"
 
 	])
 	.run([function () {
--- public/app/routes.js
+++ public/app/routes.js
@@ -1,5 +1,5 @@
 (function(angular) {
-	angular.module("wcm-arcgis-sna_1.1.0")
+	angular.module("wcm-arcgis-sna_1.2.0")
 		.config([
 
 			"$stateProvider",
